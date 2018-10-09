dd5418c98ddc38317b2407fb460f92c0c42981a8
Beth Jackson <Mbeth.ashton@gmail.com>
2018-10-09 10:11:01 -0700

    Weird bug during construction
    Place a house on an odd number of trees (so the points required will be either 25 or 35). BuildingQueue Length is messed up somehow.

--- src/date.js
+++ src/date.js
@@ -26,10 +26,15 @@ const DateView = {
         }
 
         //apply points if there is anything in the queue
-        if (State.buildingQueue.length > 0){ 
-            DateView.applyPoints(MapView);    
+        if (State.buildingQueue.length > 0){
+            console.log ('buildingQueue length at beginning: ', State.buildingQueue.length);
+            
+            console.log ('buildingQueue: at beginning', State.buildingQueue);
+            DateView.applyPoints(MapView);
+            console.log ('buildingQueue length at end of month: ', State.buildingQueue.length);
+            console.log ('buildingQueue: at end', State.buildingQueue);
         }
-        DateView.gathering();
+        //DateView.gathering();
 
         DateView.setDateText();
         DateView.displayAlerts();
@@ -62,12 +67,14 @@ const DateView = {
     applyPoints(MapView){
         const builders = State.findPeepsByJob('builder')
         let buildPoints = 0
+        let buildPointsUsed = 0
 
         for (const builder of builders){
             buildPoints += (10 + builder.buildSkill);
         };
 
         while (buildPoints > 0){
+
             firstItem = DateView.minBy(State.buildingQueue, item => item.QueueOrder)
             firstItemIndex = State.buildingQueue.indexOf(firstItem);
             
@@ -76,52 +83,67 @@ const DateView = {
 
             if (firstItemPoints <= buildPoints){
                 buildPoints -= firstItemPoints;
+                
+                //count how many points are used this month
+                buildPointsUsed += firstItemPoints;
+
                 //update structure list
                 firstItemStructure.pointsLeft = firstItemPoints;
                 //update structure_con to finished version. (how to handle multiple kinds of construction?)
                 MapView.updateBuilding(firstItemStructure.structureNum);
                 //remove item from queue
-                console.log(State.buildingQueue);
                 State.buildingQueue.splice(firstItemIndex, 1);
-                console.log(State.buildingQueue);
                 //update queue order
-                State.buildingQueue = DateView.updateQueue(State.buildingQueue);
-                
+                if (State.buildingQueue.length > 0){
+                    State.buildingQueue = DateView.updateQueue(State.buildingQueue);
+                }
             }
             if (firstItemPoints > buildPoints){
                 firstItemPoints -= buildPoints;
+                
+                //count how many points are used this month
+                buildPointsUsed += buildPoints;
+
                 buildPoints = 0;
                 //update structure list
                 firstItemStructure.pointsLeft = firstItemPoints;
             }
         }
+
+        console.log ('Build Points used this month', buildPointsUsed);
+
+        //level up builders
+        //A = total points used by all the builders
+        //B = total builders assigned
+        //C = ridiculous equation (0.0000467346938776LEVEL^2-0.00746510204082LEVEL+0.257418367347)/10
+        //X = Peep[i] gets this much XP
         
     },
-    distance(x1, x2, y1, y2){
+    // distance(x1, x2, y1, y2){
         
-    },
+    // },
 
-    closestTrees(originRow, originCol){
-        var trees = [], i = -1;
-        for(i=0; i < State.structures.length; i++){
-            if (State.structures[i].type === 'tree'){
-                trees.push(State.structures[i]);
-            }
-        }
-        return trees;
-    },
+    // closestTrees(originRow, originCol){
+    //     var trees = [], i = -1;
+    //     for(i=0; i < State.structures.length; i++){
+    //         if (State.structures[i].type === 'tree'){
+    //             trees.push(State.structures[i]);
+    //         }
+    //     }
+    //     return trees;
+    // },
 
-    gathering() {
-        const gatherers = State.findPeepsByJob('gatherer');
-        for (gatherer of gatherers){
-            houseNum = gatherer.house;
+    // gathering() {
+    //     const gatherers = State.findPeepsByJob('gatherer');
+    //     for (gatherer of gatherers){
+    //         houseNum = gatherer.house;
             
-            console.log (gatherer);
-            houseX = State.findMapCoordsByHouse(houseNum).x;
-            houseY = State.findMapCoordsByHouse(houseNum).y;
-            console.log (houseX, houseY)
-        }
-    },
+    //         console.log (gatherer);
+    //         houseX = State.findMapCoordsByHouse(houseNum).x;
+    //         houseY = State.findMapCoordsByHouse(houseNum).y;
+    //         console.log (houseX, houseY)
+    //     }
+    // },
 
     displayAlerts() {
         if (State.currentMonth in alerts) {
