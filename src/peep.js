769250e9bc5ec821b868ace4f92557827dd7349e
Beth Ashton <Mbeth.ashton@gmail.com>
2018-12-18 01:22:42 -0800

    Peeps now have a page.
    Mic Drop. Boom Shakalaka. Alakazam. Boo-yah
    
    Couple of weird little bugs (like the map moves), but it works, it communicates easily without having to worry about states or ipc or whatever.

--- src/peep.js
+++ src/peep.js
@@ -1,21 +1,42 @@
 
 const State = require('./state');
-//const peepsMenu = document.getElementById(peepsMenu)
-
 
 const PeepsView = {
 
   init(peeps_button, peepsMenu){
-    peeps_button.addEventListener('click', PeepsView.toggleMenu(peepsMenu))
+    document.getElementById('peepsList').appendChild(PeepsView.peepsList())
+    PeepsView.peepsList();
+    peeps_button.addEventListener('click', PeepsView.toggleMenu);
+    peepsMenu.style.display = "none";
   },
 
-  toggleMenu(peepsMenu){
+  toggleMenu(){
     console.log ("button pressed");
     if (peepsMenu.style.display === "none"){
       peepsMenu.style.display = "block"
     } else {
       peepsMenu.style.display = "none"
     }
+  },
+
+  peepsList(){
+    console.log (State.peeps);
+    //create the list element:
+    var list = document.createElement('ul');
+
+    for (var i=0; i < State.peeps.length; i++) {
+      //create the list item
+      var item = document.createElement('li');
+
+      //set its contents:
+      item.appendChild(document.createTextNode(State.peeps[i].name));
+
+      //add it to the list:
+      list.appendChild(item);
+    }
+
+    //return the constructed list:
+    return list;
   }
 
 }
