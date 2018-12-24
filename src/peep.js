0ffb5354eef6c281ffca2db4bd052b54b6e4aaa8
Beth Ashton <Mbeth.ashton@gmail.com>
2018-12-24 15:31:53 -0800

    Peeps List is now inn a table
    With name, age, and job

--- src/peep.js
+++ src/peep.js
@@ -5,14 +5,21 @@ const PeepsView = {
 
   init(peeps_button, peepsMenu){
     document.getElementById('peepsList').appendChild(PeepsView.peepsList())
-    PeepsView.peepsList();
     peeps_button.addEventListener('click', PeepsView.toggleMenu);
     peepsMenu.style.display = "none";
   },
 
   toggleMenu(){
     console.log ("button pressed");
+    let list = document.getElementById('peepsList')
+
     if (peepsMenu.style.display === "none"){
+      while (list.firstChild) {
+        list.removeChild(list.firstChild);
+      }
+  
+      list.appendChild(PeepsView.peepsList())
+
       peepsMenu.style.display = "block"
     } else {
       peepsMenu.style.display = "none"
@@ -20,19 +27,38 @@ const PeepsView = {
   },
 
   peepsList(){
-    console.log (State.peeps);
     //create the list element:
-    var list = document.createElement('ul');
+    var list = document.createElement('table');
+    console.log(list);
+    list.setAttribute("id", "peepsTable");
 
     for (var i=0; i < State.peeps.length; i++) {
-      //create the list item
-      var item = document.createElement('li');
+      //create the list item (insertRow()?)
+      var row = document.createElement('tr');
 
-      //set its contents:
-      item.appendChild(document.createTextNode(State.peeps[i].name));
+      //name
+      var cell1 = document.createElement("td");
+      var name = document.createTextNode(State.peeps[i].name)
+
+      cell1.appendChild(name);
+      row.appendChild(cell1);
+
+      //job
+      var cell2 = document.createElement("td");
+      var job = document.createTextNode(State.peeps[i].job)
+
+      cell2.appendChild(job);
+      row.appendChild(cell2);
+
+      //age
+      var cell3 = document.createElement("td");
+      var age = document.createTextNode(State.peeps[i].age)
+
+      cell3.appendChild(age);
+      row.appendChild(cell3);
 
       //add it to the list:
-      list.appendChild(item);
+      list.appendChild(row);
     }
 
     //return the constructed list:
